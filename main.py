2f6f83a3dc6c985e31e81c37b00f02cf269227e2
nabeelsherazi <nabeelsherazi97@gmail.com>
2017-10-11 23:31:18 -0400

    finished setup helper, fleshing out main function

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-from classes import Student, Session
+import os
 import pickle
+from classes import Student, Session, TransactionRecord
+import prettyCLI as pretty
 
 
-if __name__ == '__main__':
+def setup():
+    """
+    Setup function to checks if transaction data exists, otherwise create a
+    new file. Returns either TransactionRecord object in pickled data or new
+    TransactionRecord.
+    """
+    for (cdir, sdirs, fs) in os.walk("data"):
+        if records.pkl not in fs:
+            print("Data file not found... creating now.")
+            with open("data\\records.pkl", "wb") as f:
+                pass
     try:
-        with open("data\\data.dat", "rb") as f:
-            pass
-    except FileNotFoundError:
-        print("Data file not found... creating now.")
-        with open("data\\data.dat", "wb") as f:
-            pass
+        with open("data\\students.pkl", "rb") as f:
+            active_students = pickle.load(f)  # Might add feature for more student lists later.
+            print("Student list loaded successfully.")
 
-    with open("data\\data.dat", "rb") as f:
-        data = pickle.load(f)
+        with open("data\\records.pkl", "rb") as f:
+            active_record = pickle.load(f)  # Might add feature for more records later.
+            print("Transaction record loaded successfully.")
+    except EOFError:
+        # If first time running, pickle file will be blank, so return new
+        # empty student list and transaction record.
+        print("New student list and transaction record created successfully.")
+        return (StudentList(), TransactionRecord())
+    else:
+        return (active_students, active_record)
+
+
+def main(student_list, transaction_record):
+    """
+    Main function. Accepts an active record and provides a nice walk through to
+    faciliate recordkeeping actions.
+    """
+    pretty.marquee("Welcome to Tutor Stuff (name pending)")
     
