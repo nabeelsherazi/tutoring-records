56b1345a25b0902d3c7a1ac82f5a0d8168fde5ad
nabeelsherazi <nabeelsherazi97@gmail.com>
2017-10-21 18:36:12 -0400

    inital commit - new helper module to handle string validation that was being repeated everywhere

---
+++ field_validation.py
@@ -0,0 +1,48 @@
+"""
+Helper module to handle string validation stuff that I noticed I had to do
+everywhere. It is a given for all functions that they will only be handed strings
+(thus no type checking is necessary).
+"""
+
+import string
+
+
+def is_letter(string):
+    """
+    Checks if provided string is a single letter.
+    Examples:
+    "a" --> True
+    "C" --> True
+    "2" --> False
+    """
+    if string not in string.ascii_letters or len(string) > 1:
+        return False
+    return True
+
+
+def is_date(string):
+    """
+    Checks if provided string is a valid date. Valid in this case also means that
+    month and day can be either zero padded or not, but year may not be
+    abbreviated. However, will NOT check for value ranges in date.
+    Examples:
+    "10/21/2017" --> True
+    "4/1/1988" --> True
+    "05/08/2015" --> True
+    "5/68/2000" --> True
+    "04201966" --> False
+    """
+    date = string.split("/")
+    if len(date) != 3:
+        return False
+    if len(date[2]) != 4:
+        return False
+    return True
+
+
+def is_bool(string):
+    """ Checks if provided string has some kind of a boolean meaning. """
+    bools = ["yes", "y", "1", "no", "n", "0"]
+    if string.lower() in bools:
+        return True
+    return False
