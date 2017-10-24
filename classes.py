896363b2daedd508703c7b551aebb618cce8bf98
nabeelsherazi <nabeelsherazi97@gmail.com>
2017-10-24 06:50:29 -0400

    finish implementation of printing in StudentList, began imp of printing in TransactionRecord

--- classes.py
+++ classes.py
@@ -8,6 +8,7 @@ import prettyCLI as pretty
 # does CLI interfacing. All object creation should be handled inside of the class
 # when it's passed a string. I should be able to pass Session a string for
 # session date and have it create the datetime.date object within itself.
+# Update: I think this is a bad idea.
 
 
 class Student:
@@ -61,15 +62,15 @@ class Session:
     """
     Defines tutoring session data type.
 
-    Must provide:
+    All paramters will be given as STRINGS and then internally converted into:
     date -> datetime.date object
     start_time, end_time -> datetime.time objects
     rate -> float
     pmt_method -> string
-    pmt_recieved -> boolean
+    pmt_received -> boolean
     notes -> string
     """
-    def __init__(self, date, student, start_time, end_time, rate, pmt_method, pmt_recieved, notes):
+    def __init__(self, date, student, start_time, end_time, rate, pmt_method, pmt_received, notes):
         #  Variable assignments
         self.date = date
         self.student = student
@@ -77,17 +78,16 @@ class Session:
         self.end = datetime.datetime.combine(date, end_time)
         self.rate = rate
         self.method = pmt_method.upper()
-        self.recieved = pmt_recieved
+        self.received = pmt_received
         self.notes = notes
         #  Subtracting two datetime.datetime objects creates a new
         #  datetime.timedelta, of which we take the number of seconds attribute
         #  and floor divide by 3600 to get the number of billable hours in the
         #  session.
-        self.duration = ((self.end - self.start).seconds) // 3600
-        self.charge = self.duration * self.rate
+        self.calculate_charge()
 
     def __str__(self):
-        return "Session on {0} with {1} from {2} to {3} ({4} hours @ ${5}/hr), total charges ${6} by {7}, {8}. Notes: {9} ".format(str(self.date), str(self.student), str(self.start), str(self.end), self.duration, self.rate, self.charge, self.method, "payment recieved" if self.received else "payment NOT received", self.notes)
+        return "Session on {0} with {1} from {2} to {3} ({4} hours @ ${5}/hr), total charges ${6} by {7}, {8}. Notes: {9} ".format(str(self.date), str(self.student), str(self.start), str(self.end), self.duration, self.rate, self.charge, self.method, "payment received" if self.received else "payment NOT received", self.notes)
 
     def __lt__(self, other):
         return self.start < other.start
@@ -107,6 +107,10 @@ class Session:
     def __ne__(self, other):
         return self.start != other.start
 
+    def calculate_charge(self):
+        self.duration = ((self.end - self.start).seconds) // 3600
+        self.charge = self.duration * self.rate
+
     def update(self, field, newval):
         """Updates session field, specified as string, to newval."""
         try:
@@ -115,6 +119,7 @@ class Session:
             return False
         else:
             self.__dict__[field] = newval
+            self.calculate_charge()  # In case of change to rate
             return True
 
 
@@ -174,6 +179,63 @@ class TransactionRecord:
                 return s
         return False
 
+    def print_table(self, start=None, end=None):
+        """ Prints self """
+        # Get subsection to print
+        start_ix = end_ix = None
+        if len(self.transactions) == 0:
+            print("No transactions on record.")
+            return
+        if end < start:
+            raise ValueError("Invalid start/end pair.")
+        for i, s in enumerate(self.transactions):
+            if s.date >= start:  # Start printing at the first name after our start point
+                start_ix = i
+                break
+        if start_ix is None:
+            start_ix = len(self.transactions) - 1
+        for i, s in enumerate(self.transactions):
+            if s.date > end:  # Stop printing at one before first name after end
+                end_ix = i - 1
+                break
+        if end_ix is None:
+            end_ix = len(self.transactions) - 1
+
+        to_print = self.transactions[start_ix:end_ix]
+
+        # Print head
+        col_spans = [10, 20, 10, 10, 7, 7, 7, 20, 15, 15]
+        for session in to_print:  # Actually only need the first one
+            assert len(col_spans) == len(session.__dict__)  # This is for you!
+            for head, span in zip(session.__dict__, col_spans):  # Zips each
+                # heading and span length as a tuple and then unpacks it into the for loop
+                # so they can be accessed together exclusively... I'm clever, see!
+                trimmed_head = head[0:(span - 2)] + ("." if len(head) > (span - 1) else "")
+                # Does nothing if column span is greater than heading length + 2 spaces,
+                # but slices and abbreviates with "." if not.
+                print(trimmed_head.capitalize() + (" " * (span - len(trimmed_head))), end="")
+            break
+        print()
+        pretty.print_bar(80)
+
+        # Print student info
+        for session in to_print:
+            for head, span in zip(session.__dict__, col_spans):
+                if head == "start" or head == "end":
+                    info = pretty.time(session.__dict__[head])
+                else:
+                    info = str(session.__dict__[head])
+                # The trimmed info will be a slice plus a dash if the length of the original info
+                # was greater than the column span.
+                trimmed_info = info[0:(span - 2)] + ("-" if len(info) > (span - 2) else "")
+                print(trimmed_info + (" " * (span - len(trimmed_info))), end="")
+            print()
+
+
+
+
+
+
 
 class StudentList:
     """
@@ -228,27 +290,38 @@ class StudentList:
                 return s
         return False
 
-    def print_table(start="a", end="z"):
+    def print_table(self, start="a", end="z"):
         """ Prints self """
         # Get subsection to print
         start, end = start.lower(), end.lower()
+        start_ix = end_ix = None
+        if len(self.students) == 0:
+            print("No students in list.")
+            return
         if end < start:
-            raise ValueError
+            raise ValueError("Invalid start/end pair.")
         for i, s in enumerate(self.students):
             if s.first[0].lower() >= start:  # Start printing at the first name after our start point
                 start_ix = i
                 break
+        if start_ix is None:
+            start_ix = len(self.students) - 1
         for i, s in enumerate(self.students):
             if s.first[0].lower() > end:  # Stop printing at one before first name after end
                 end_ix = i - 1
+                break
+        if end_ix is None:
+            end_ix = len(self.students) - 1
+
         to_print = self.students[start_ix:end_ix]
 
         # Print head
-        col_spans = [10, 20, 10, 10, 5, 5, 5, 15]
+        col_spans = [15, 15, 12, 20, 18, 20]
         for student in to_print:  # Actually only need the first one
-            for head, span in zip(student.__dict__, col_spans):
-                assert len(col_spans) == len(student.__dict__)  # This is for you!
-                # Remember each h has both its text in h[0] and its desired length in h[1].
+            assert len(col_spans) == len(student.__dict__)  # This is for you!
+            for head, span in zip(student.__dict__, col_spans):  # Zips each
+                # heading and span length as a tuple and then unpacks it into the for loop
+                # so they can be accessed together exclusively... I'm clever, see!
                 trimmed_head = head[0:(span - 2)] + ("." if len(head) > (span - 1) else "")
                 # Does nothing if column span is greater than heading length + 2 spaces,
                 # but slices and abbreviates with "." if not.
