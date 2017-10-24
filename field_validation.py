669e64ef84ec785514768bc47e64ab03fb4d29c5
nabeelsherazi <nabeelsherazi97@gmail.com>
2017-10-24 06:52:56 -0400

    fixed bug

--- field_validation.py
+++ field_validation.py
@@ -7,7 +7,7 @@ everywhere. It is a given for all functions that they will only be handed string
 import string
 
 
-def is_letter(string):
+def is_letter(string_):
     """
     Checks if provided string is a single letter.
     Examples:
@@ -15,12 +15,12 @@ def is_letter(string):
     "C" --> True
     "2" --> False
     """
-    if string not in string.ascii_letters or len(string) > 1:
+    if string_ not in string.ascii_letters or len(string_) > 1:
         return False
     return True
 
 
-def is_date(string):
+def is_date(string_):
     """
     Checks if provided string is a valid date. Valid in this case also means that
     month and day can be either zero padded or not, but year may not be
@@ -32,7 +32,7 @@ def is_date(string):
     "5/68/2000" --> True
     "04201966" --> False
     """
-    date = string.split("/")
+    date = string_.split("/")
     if len(date) != 3:
         return False
     if len(date[2]) != 4:
@@ -40,9 +40,18 @@ def is_date(string):
     return True
 
 
-def is_bool(string):
+def is_bool(string_):
     """ Checks if provided string has some kind of a boolean meaning. """
     bools = ["yes", "y", "1", "no", "n", "0"]
-    if string.lower() in bools:
+    if string_.lower() in bools:
         return True
     return False
+
+
+def is_rate(string_):
+    try:
+        float(string_)
+    except ValueError:
+        return False
+    else:
+        return True
